70a380fd61629c8395f53b7abfb07d3597bfea71
Vadim Kravcov <v.sem4ik@mail.ru>
2023-07-26 14:12:01 +0300

    add fixies homework 15.1

--- src/video.py
+++ src/video.py
@@ -11,6 +11,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Video:
 
+
     def __init__(self, video_id):
         self.video_id = video_id
 
@@ -22,20 +23,18 @@ class Video:
         self.quantity_all_views = video_response['items'][0]['statistics']['viewCount']
         self.like_count = video_response['items'][0]['statistics']['likeCount']
 
+
     def __str__(self):
         return self.video_title
 
 
-# video_id = 'gaoc9MPZ4bw'
-# video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-#                                                id=video_id
-#                                                ).execute()
 
+class PLVideo(Video):
 
-class PLVideo:
 
     def __init__(self, video_id, playlist_id):
-        self.video_id = video_id
+        super().__init__(video_id)
+        # self.video_id = video_id
         self.playlist_id = playlist_id
 
         video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
@@ -46,30 +45,10 @@ class PLVideo:
         self.quantity_all_views = video_response['items'][0]['statistics']['viewCount']
         self.like_count = video_response['items'][0]['statistics']['likeCount']
 
+
     def __str__(self):
         return self.video_title
 
-'''
-получить статистику видео по его id
-получить id можно из адреса видео
-https://www.youtube.com/watch?v=gaoc9MPZ4bw или https://youtu.be/gaoc9MPZ4bw
-'''
-# video_id = 'gaoc9MPZ4bw'
-# video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-#                                        id=video_id
-#                                        ).execute()
-# printj(video_response)
-
-
-# video_title: str = video_response['items'][0]['snippet']['title']
-# view_count: int = video_response['items'][0]['statistics']['viewCount']
-# like_count: int = video_response['items'][0]['statistics']['likeCount']
-# comment_count: int = video_response['items'][0]['statistics']['commentCount']
-#
-#
-# print(video_response)
-#
-# print(video_response['items'][0]['statistics']['likeCount'])
 
 
 
